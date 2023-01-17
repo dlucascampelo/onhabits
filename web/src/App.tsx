a78e72ee9060d8afe37dd6aa622add3a90b5b38f
Lucas Campelo <dvlucascampelo@gmail.com>
2023-01-17 04:04:37 -0300

    feat: web enviroment setup

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './styles/global.css';
+
+function App() {
+
+  return (
+    <div className='bg-zinc-900 w-1920 h-100vh'>
+      <h1 className="text-white rounded text-3xl">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
